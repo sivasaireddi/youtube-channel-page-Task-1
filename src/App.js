410cd07ccbff636f588c9660e5d9a817b02f5931
sivasaireddi <sainimmakayala123@gmail.com>
2024-05-16 16:16:00 +0530

    youtubeTasks

---
+++ src/App.js
@@ -0,0 +1,91 @@
+
+
+function App() {
+  return (
+    <div>
+      <header style={{
+      display:'flex',
+        gap:'20px'
+      }}>
+      <img  style={{
+         borderRadius:"50%"
+
+      }}
+      src="https://yt3.googleusercontent.com/1FEdfq3XpKE9UrkT4eOc5wLF2Bz-42sskTi0RkK4nPh4WqCbVmmrDZ5SVEV3WyvPdkfR8sw2=s160-c-k-c0x00ffffff-no-rj" alt=""/>
+      <div>
+        <h2> Chai aur Code</h2>
+        <ul style={{
+          display:'flex',
+          gap:'20px'
+        }}
+        type='none'>
+          <li>@chaiaurcode</li>
+          <li>285K subscribers</li>
+          <li>353 videos</li>
+        </ul>
+        <p>
+        A channel dedicated to chai and coding in HINDI. A lot happens over chai and I am a big-time chai lover. 
+        </p>
+        
+      </div>
+      <a href="">chaicode.com
+</a>and 1 more link
+      <div>
+        <div >
+          <button style={{
+            padding:'8px 20px',
+          borderRadius:'15px',
+          border:'none',
+          backgroundColor:'black',
+          color:'white',
+          marginRight:'20px',
+          fontWeight:'600'}}>subscribe</button>
+          <button style={{
+            padding:'8px 20px',
+          borderRadius:'15px',
+          border:'2px solid grey',
+          fontWeight:'600',
+          backgroundColor:'white'}}>join</button>
+        </div>
+
+      </div>
+      </header>
+      <hr/>
+      
+      <section >
+<h2> For You</h2>
+<div style={{
+        marginTop:'50px',
+        display:'flex',
+        gap:'30px',
+        
+        
+      }}>
+<article>
+  <img src="https://i.ytimg.com/an_webp/66lfSjIrG8M/mqdefault_6s.webp?du=3000&sqp=CJXBlrIG&rs=AOn4CLCAkG292PsabLeUFuaRufq_fDwIUw" alt=''
+  style={{borderRadius:'20px'}}/>
+  <h4>Let's build a meme generator</h4>
+  <p>6.3K views
+1 day ago</p>
+</article>
+<article>
+  <img src="https://i.ytimg.com/an_webp/66lfSjIrG8M/mqdefault_6s.webp?du=3000&sqp=CJXBlrIG&rs=AOn4CLCAkG292PsabLeUFuaRufq_fDwIUw" alt=''
+    style={{borderRadius:'20px'}}/>
+  <h4>Let's build a meme generator</h4>
+  <p>6.3K views
+1 day ago</p>
+</article>
+<article>
+  <img src="https://i.ytimg.com/an_webp/66lfSjIrG8M/mqdefault_6s.webp?du=3000&sqp=CJXBlrIG&rs=AOn4CLCAkG292PsabLeUFuaRufq_fDwIUw" alt=''
+    style={{borderRadius:'20px'}}/>
+  <h4>Let's build a meme generator</h4>
+  <p>6.3K views
+1 day ago</p>
+</article>
+</div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
